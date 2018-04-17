bb8cb21fbc665187dab9132322ecc26701c68295
Daniel Mancera <daniel.mancera@crg.eu>
2018-04-17 11:39:20 +0200

    set the current timezone to Europe/Madrid

--- src/main/java/eu/qcloud/QCloudApplication.java
+++ src/main/java/eu/qcloud/QCloudApplication.java
@@ -1,10 +1,19 @@
 package eu.qcloud;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QCloudApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(QCloudApplication.class, args);
