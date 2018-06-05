622edf05044d5b49d7de79404d43905e62177d28
Daniel Mancera <daniel.mancera@crg.eu>
2018-06-05 15:12:57 +0200

    add complexity function

--- src/main/java/eu/qcloud/sampleTypeCategory/SampleTypeCategoryRepository.java
+++ src/main/java/eu/qcloud/sampleTypeCategory/SampleTypeCategoryRepository.java
@@ -1,5 +1,6 @@
 package eu.qcloud.sampleTypeCategory;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,6 +8,8 @@ import org.springframework.data.repository.CrudRepository;
 public interface SampleTypeCategoryRepository extends CrudRepository<SampleTypeCategory, Long> {
 
 	Optional<SampleTypeCategory> findByName(String name);
+
+	List<SampleTypeCategory> findBySampleTypeComplexity(SampleTypeComplexity complexity);
 	
 	
 
