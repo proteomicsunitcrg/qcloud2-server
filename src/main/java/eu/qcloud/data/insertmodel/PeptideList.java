7dcac54b9003e13dc80b06edd6069010857b4019
Daniel Mancera <daniel.mancera@crg.eu>
2018-06-12 15:09:05 +0200

    add model for insert peptides in the database

---
+++ src/main/java/eu/qcloud/data/insertmodel/PeptideList.java
@@ -0,0 +1,25 @@
+package eu.qcloud.data.insertmodel;
+
+public class PeptideList {
+	
+	private String sequence;
+	
+	private Float value;
+
+	public String getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(String sequence) {
+		this.sequence = sequence;
+	}
+
+	public Float getValue() {
+		return value;
+	}
+
+	public void setValue(Float value) {
+		this.value = value;
+	}
+	
+}
