e6b4029c7a6d20a66ed52ba6980fc6d30180cc97
Daniel Mancera <daniel.mancera@crg.eu>
2018-06-13 11:55:29 +0200

    add a query to look for a sample composition

--- src/main/java/eu/qcloud/sampleComposition/SampleCompositionRepository.java
+++ src/main/java/eu/qcloud/sampleComposition/SampleCompositionRepository.java
@@ -2,6 +2,7 @@ package eu.qcloud.sampleComposition;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import eu.qcloud.contextSource.peptide.Peptide;
@@ -11,6 +12,9 @@ public interface SampleCompositionRepository extends CrudRepository<SampleCompos
 	public List<SampleComposition> findByPeptideId(Long peptideId);
 	
 	public List<PeptidesFromSample> findBySampleTypeName(String name);
+	
+	@Query("select sc from SampleComposition sc where sc.sampleType.id=?1 and sc.peptide.id = ?2")
+	public SampleComposition getSampleCompositionBySampleTypeIdAndPeptideId(Long sampleTypeId, Long peptideId);
 	
 	
 	interface PeptidesFromSample {
