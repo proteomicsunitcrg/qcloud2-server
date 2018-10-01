1c376f9446e4b82ca078a351ea4c503592e65010
Daniel Mancera <daniel.mancera@crg.eu>
2018-10-01 11:45:41 +0200

    add migrations

---
+++ src/main/resources/db/migration/V6__peptides_hela_qc4l.sql
@@ -0,0 +1,48 @@
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: qcloud2
+-- ------------------------------------------------------
+-- Server version	5.7.23-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `context_source`
+--
+
+LOCK TABLES `context_source` WRITE;
+/*!40000 ALTER TABLE `context_source` DISABLE KEYS */;
+INSERT INTO `context_source` VALUES ('peptide',17,'YAE',_binary '*ü=6CE?°O@gcGµï','YAEAVTR',NULL,2,405.211,'YAEAVTR'),('peptide',18,'TPA',_binary '∆≥06s\"Oô¢ ±)\‡	óE','TPAQFDADELR',NULL,2,631.804,'TPAQFDADELR'),('peptide',19,'STL',_binary '≈Ä,áóFò\”O¶ª“µ','STLTDSLVC(Carbamidomethyl)K',NULL,2,562.287,'STLTDSLVC(Carbamidomethyl)K'),('peptide',20,'SLA',_binary '9Zï\‘@%I•©\»c$x\Ë','SLADELALVDVLEDK',NULL,2,815.433,'SLADELALVDVLEDK'),('peptide',21,'NPD',_binary 'IÖ¨\∆G\\îÉ\≈˛=ë˘ó','NPDDITNEEYGEFYK',NULL,2,917.394,'NPDDITNEEYGEFYK'),('peptide',22,'LGD',_binary '4HE98A\ZÑùFJ\«\ﬂ','LGDLYEEEMR',NULL,2,627.787,'LGDLYEEEMR'),('peptide',23,'LAV',_binary '!Ωa\‡úJN±∂:\≈ò?\‡','LAVDEEENADNNTK',NULL,2,781.352,'LAVDEEENADNNTK'),('peptide',24,'FEE',_binary '\Ÿ˝\n±DãJ±´Éõ?\Á\Ÿ\Ô\‹','FEELNMDLFR',NULL,2,657.313,'FEELNMDLFR'),('peptide',25,'EAA',_binary '\»R~π\rüLYµ\œ\ﬁˇ\“[í','EAALSTALSEK',NULL,2,560.298,'EAALSTALSEK'),('peptide',26,'DDV',_binary 'c)øJ“π\ŒıäkA\ZÙ','DDVAQTDLLQIDPNFGSK',NULL,2,988.484,'DDVAQTDLLQIDPNFGSK'),('peptide',27,'RFP',_binary '\È4\ÕKvJN¢\‚®⁄≠ú´','RFPGYDSESK',NULL,2,593.28,'RFPGYDSESK'),('peptide',28,'EVS',_binary '\√M˙æ\»E◊õ{sâv\’ˆ\'','EVSTYIK',NULL,2,420.229,'EVSTYIK'),('peptide',29,'EAT',_binary 'är\«21\"D6ã;\ÁT\"p]','EATTEFSVDAR',NULL,2,613.288,'EATTEFSVDAR'),('peptide',30,'FAF',_binary 'µ1E\Ó†\ÏOWß∑ûeZÄØ∑','FAFQAEVNR',NULL,2,541.275,'FAFQAEVNR'),('peptide',31,'EQF',_binary '\ \'U\È8Jé®≠\Ë\Ê†]ç\Â','EQFLDGDGWTSR',NULL,2,705.818,'EQFLDGDGWTSR'),('peptide',32,'YVY',_binary '9mWY3&L®î\”*6ha0','YV(Heavy)YV(Heavy)ADV(Heavy)A(Heavy)A(Heavy)K(Heavy)',NULL,2,566.83,'YV(Heavy)YV(Heavy)ADV(Heavy)A(Heavy)A(Heavy)K(Heavy)'),('peptide',33,'YVY',_binary '\⁄Wñ$`J≤í!≥`Cç','YV(Heavy)YV(Heavy)ADV(Heavy)AAK(Heavy)',NULL,2,562.823,'YV(Heavy)YV(Heavy)ADV(Heavy)AAK(Heavy)'),('peptide',34,'YVY',_binary '\«PìG\√\\Aﬁ∑º1ø®æ˚','YVYV(Heavy)ADV(Heavy)AAK(Heavy)',NULL,2,559.816,'YVYV(Heavy)ADV(Heavy)AAK(Heavy)'),('peptide',35,'YVY',_binary 'g8\Ï´jKŒú\0\'n˝Çπ','YVYVADV(Heavy)AAK(Heavy)',NULL,2,556.809,'YVYVADV(Heavy)AAK(Heavy)'),('peptide',36,'YVY',_binary 'î\Í<vﬁ™F—∫\«\·¨Èû©r','YVYVADVAAK(Heavy)',NULL,2,553.802,'YVYVADVAAK(Heavy)'),('peptide',37,'LLS',_binary 'ÖS\\\›,IGdúà	éíëwK','L(Heavy)L(Heavy)SL(Heavy)GAGEF(Heavy)K(Heavy)',NULL,2,537.344,'L(Heavy)L(Heavy)SL(Heavy)GAGEF(Heavy)K(Heavy)'),('peptide',38,'LLS',_binary '\Ë\’äZFIâÑ\‰p\◊\¬\›','L(Heavy)L(Heavy)SL(Heavy)GAGEFK(Heavy)',NULL,2,532.331,'L(Heavy)L(Heavy)SL(Heavy)GAGEFK(Heavy)'),('peptide',39,'LLS',_binary 'zST\◊MÅA0ù/T\„t?Æ','L(Heavy)L(Heavy)SLGAGEFK(Heavy)',NULL,2,528.822,'L(Heavy)L(Heavy)SLGAGEFK(Heavy)'),('peptide',40,'LLS',_binary '\€\¬g§x@Ùü˜Úø\Â\–4','L(Heavy)LSLGAGEFK(Heavy)',NULL,2,525.313,'L(Heavy)LSLGAGEFK(Heavy)'),('peptide',41,'LLS',_binary '∂íÄ@YH)ØHNÑ\Á\„ı','LLSLGAGEFK(Heavy)',NULL,2,521.805,'LLSLGAGEFK(Heavy)'),('peptide',42,'LGF',_binary '\‚\ÂP-yM+º∂Äñ\ﬂ¬úˇ','L(Heavy)GF(Heavy)TDL(Heavy)F(Heavy)SK(Heavy)',NULL,2,535.328,'L(Heavy)GF(Heavy)TDL(Heavy)F(Heavy)SK(Heavy)'),('peptide',43,'LGF',_binary '[K\ÎDB¢\»ˇÃøQUâ','L(Heavy)GFTDL(Heavy)F(Heavy)SK(Heavy)',NULL,2,530.315,'L(Heavy)GFTDL(Heavy)F(Heavy)SK(Heavy)'),('peptide',44,'LGF',_binary '\À!\∆\Ã˝\ÀHò∂?Sø4∏\Í','L(Heavy)GFTDL(Heavy)FSK(Heavy)',NULL,2,525.301,'L(Heavy)GFTDL(Heavy)FSK(Heavy)'),('peptide',45,'LGF',_binary '\¬bä\Á\’OµQ∑€Ω\÷7','L(Heavy)GFTDLFSK(Heavy)',NULL,2,521.792,'L(Heavy)GFTDLFSK(Heavy)'),('peptide',46,'LGF',_binary '£ıvßàCµ\ﬂ#±π(','LGFTDLFSK(Heavy)',NULL,2,518.284,'LGFTDLFSK(Heavy)'),('peptide',47,'VTS',_binary '±?ˆñYM≈ø£t\Ï\Á¿Ò¡','V(Heavy)T(Heavy)S(Heavy)GST(Heavy)ST(Heavy)SR(Heavy)',NULL,2,509.274,'V(Heavy)T(Heavy)S(Heavy)GST(Heavy)ST(Heavy)SR(Heavy)'),('peptide',48,'VTS',_binary 'Zf5Dé\„\„U\ ÒG','V(Heavy)T(Heavy)SGSTST(Heavy)SR(Heavy)',NULL,2,504.765,'V(Heavy)T(Heavy)SGSTST(Heavy)SR(Heavy)'),('peptide',49,'VTS',_binary '\Â µ€ÅN1ºòö;WfÇ\¬','V(Heavy)T(Heavy)SGSTSTSR(Heavy)',NULL,2,502.26,'V(Heavy)T(Heavy)SGSTSTSR(Heavy)'),('peptide',50,'VTS',_binary '\ BÉl>\ËI¬êÄbå˛G\Ë','V(Heavy)TSGSTSTSR(Heavy)',NULL,2,499.755,'V(Heavy)TSGSTSTSR(Heavy)'),('peptide',51,'VTS',_binary '4so\ÍDBúü¨\€≈øµN¥','VTSGSTSTSR(Heavy)',NULL,2,496.748,'VTSGSTSTSR(Heavy)'),('peptide',52,'VVG',_binary '¨í ßTH(≤g%„©Ø†N','V(Heavy)V(Heavy)GGL(Heavy)V(Heavy)ALR(Heavy)',NULL,2,459.823,'V(Heavy)V(Heavy)GGL(Heavy)V(Heavy)ALR(Heavy)'),('peptide',53,'VVG',_binary 'jDN*@=Å\√\ŸˆW\«\ƒ','V(Heavy)V(Heavy)GGLV(Heavy)ALR(Heavy)',NULL,2,456.315,'V(Heavy)V(Heavy)GGLV(Heavy)ALR(Heavy)'),('peptide',54,'VVG',_binary 'ò0+9\œ*AñíEº[pùﬂæ','V(Heavy)V(Heavy)GGLVALR(Heavy)',NULL,2,453.308,'V(Heavy)V(Heavy)GGLVALR(Heavy)'),('peptide',55,'VVG',_binary 'ö\›ƒôH\‚NïÉ\'\„…ºí\0\›','V(Heavy)VGGLVALR(Heavy)',NULL,2,450.301,'V(Heavy)VGGLVALR(Heavy)'),('peptide',56,'VVG',_binary '∂§3¨¡\‹N\Ì´dgbm}}','VVGGLVALR(Heavy)',NULL,2,447.294,'VVGGLVALR(Heavy)'),('peptide',57,'LAS',_binary '®óE®ˆ>Gœï\›ú\Ó9Dí','L(Heavy)A(Heavy)SV(Heavy)SV(Heavy)S(Heavy)R(Heavy)',NULL,2,428.274,'L(Heavy)A(Heavy)SV(Heavy)SV(Heavy)S(Heavy)R(Heavy)'),('peptide',58,'LAS',_binary '7¶\Î¥>B•.,Ç\nS','L(Heavy)ASV(Heavy)SV(Heavy)SR(Heavy)',NULL,2,424.267,'L(Heavy)ASV(Heavy)SV(Heavy)SR(Heavy)'),('peptide',59,'LAS',_binary '\À(KÛ\”\√H˘µåÙ\ÍGDg','LASV(Heavy)SV(Heavy)SR(Heavy)',NULL,2,420.758,'LASV(Heavy)SV(Heavy)SR(Heavy)'),('peptide',60,'LAS',_binary '¡Ö3f\nwF@™\ÂõJÿπ%','LASVSV(Heavy)SR(Heavy)',NULL,2,417.751,'LASVSV(Heavy)SR(Heavy)'),('peptide',61,'LAS',_binary 'k+ë\ÿJ{D˜ñM•µá','LASVSVSR(Heavy)',NULL,2,414.744,'LASVSVSR(Heavy)');
+/*!40000 ALTER TABLE `context_source` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Dumping data for table `sample_composition`
+--
+
+LOCK TABLES `sample_composition` WRITE;
+/*!40000 ALTER TABLE `sample_composition` DISABLE KEYS */;
+INSERT INTO `sample_composition` VALUES (17,4,NULL),(18,4,NULL),(19,4,NULL),(20,4,NULL),(21,4,NULL),(22,4,NULL),(23,4,NULL),(24,4,NULL),(25,4,NULL),(26,4,NULL),(27,4,NULL),(28,4,NULL),(29,4,NULL),(30,4,NULL),(31,4,NULL),(32,5,100),(33,5,10),(34,5,1),(35,5,0.1),(36,5,0.01),(37,5,100),(38,5,10),(39,5,1),(40,5,0.1),(41,5,0.01),(42,5,100),(43,5,10),(44,5,1),(45,5,0.1),(46,5,0.01),(47,5,100),(48,5,10),(49,5,1),(50,5,0.1),(51,5,0.01),(52,5,100),(53,5,10),(54,5,1),(55,5,0.1),(56,5,0.01),(57,5,100),(58,5,10),(59,5,1),(60,5,0.1),(61,5,0.01);
+/*!40000 ALTER TABLE `sample_composition` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-01 11:13:17
