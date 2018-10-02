ed0a86ca17a8ecdb1f74a371e79e7719cb151ff6
Daniel Mancera <daniel.mancera@crg.eu>
2018-10-02 11:00:06 +0200

    disable swagger and actuator

--- src/main/java/eu/qcloud/swagger/config/SwaggerConfiguration.java
+++ src/main/java/eu/qcloud/swagger/config/SwaggerConfiguration.java
@@ -1,19 +1,16 @@
 package eu.qcloud.swagger.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
+// @Profile("development")
+// @Configuration
+// @EnableSwagger2
 public class SwaggerConfiguration {
 
-    @Bean
+    // @Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
