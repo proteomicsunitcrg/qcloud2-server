de2725815b2a6c1ddb6494025074977a2c7b0631
Daniel Mancera <daniel.mancera@crg.eu>
2018-10-31 12:32:55 +0100

    add fields

--- src/main/java/eu/qcloud/websocket/WebSocketNotification.java
+++ src/main/java/eu/qcloud/websocket/WebSocketNotification.java
@@ -1,8 +1,14 @@
 package eu.qcloud.websocket;
 
+import java.util.UUID;
+
 public class WebSocketNotification {
 	
 	private String action;
+	
+	private UUID apiKey;
+	
+	private String qccv;
 	
 	private Object object;
 
@@ -22,11 +28,36 @@ public class WebSocketNotification {
 		this.object = object;
 	}
 	
+	public UUID getApiKey() {
+		return apiKey;
+	}
+
+	public void setApiKey(UUID apiKey) {
+		this.apiKey = apiKey;
+	}
+	
+	public String getQccv() {
+		return qccv;
+	}
+
+	public void setQccv(String qccv) {
+		this.qccv = qccv;
+	}
+
 	public WebSocketNotification() {}
 
-	public WebSocketNotification(String action, Object object) {
+	/**
+	 * Apikey is not mandatory. Use only if needed.
+	 * @param action
+	 * @param apiKey
+	 * @param qccv
+	 * @param object
+	 */
+	public WebSocketNotification(String action, UUID apiKey, String qccv, Object object) {
 		this.action = action;
+		this.qccv = qccv;
 		this.object = object;
+		this.apiKey = apiKey;
 	}
 	
 	
