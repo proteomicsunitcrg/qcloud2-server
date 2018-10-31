171f55138dcd9d1bfa4ebdf11a312e6f2a268ca2
Daniel Mancera <daniel.mancera@crg.eu>
2018-10-31 12:33:07 +0100

    send thresholds

--- src/main/java/eu/qcloud/websocket/WebSocketService.java
+++ src/main/java/eu/qcloud/websocket/WebSocketService.java
@@ -10,59 +10,77 @@ import org.springframework.messaging.simp.user.SimpUser;
 import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Service;
 
+import eu.qcloud.data.DataForPlot;
 import eu.qcloud.labsystem.LabSystem;
 import eu.qcloud.node.Node;
 import eu.qcloud.nonconformity.thresholdnonconformity.ThresholdNonConformity;
 import eu.qcloud.param.Param;
+import eu.qcloud.sampleType.SampleType;
 import eu.qcloud.security.model.User;
 import eu.qcloud.security.repository.UserRepository;
 import eu.qcloud.utils.ThresholdUtils;
+import eu.qcloud.utils.factory.ThresholdForPlotImpl;
 
 @Service
 public class WebSocketService {
-	
+
 	@Autowired
-    private SimpMessagingTemplate messagingTemplate;
-	
+	private SimpMessagingTemplate messagingTemplate;
+
 	@Autowired
 	private SimpUserRegistry userRegistry;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private ThresholdUtils thresholdUtils;
-	
+
 	@MessageMapping("/bye")
-    public void greetingDos(Principal principal, String message) throws  Exception {
-        
-        messagingTemplate.convertAndSendToUser("dmance@outlook.es", "/queue/reply", "caca " + message);
-    }
-	
-	public void sendNonConformityToNodeUsers(Node node, List<ThresholdNonConformity> thresholdNonConformities, LabSystem labSystem) {
+	public void greetingDos(Principal principal, String message) throws Exception {
+
+		messagingTemplate.convertAndSendToUser("dmance@outlook.es", "/queue/reply", "caca " + message);
+	}
+
+	public void sendNonConformityToNodeUsers(Node node, List<ThresholdNonConformity> thresholdNonConformities,
+			LabSystem labSystem) {
 		// get connected users
-		
-		for(SimpUser s: userRegistry.getUsers()) {
+
+		for (SimpUser s : userRegistry.getUsers()) {
 			User user = userRepository.findByUsername(s.getName());
-			if(user.getNode().getId() == node.getId()) {
+			if (user.getNode().getId() == node.getId()) {
 				// send message
-				messagingTemplate.convertAndSendToUser(s.getName(),
-						"/queue/reply",
-						new WebSocketNotification("nc-" + labSystem.getApiKey(), thresholdUtils.createLabSystemStatusByThresholdNonConformity(thresholdNonConformities)));
+				messagingTemplate.convertAndSendToUser(s.getName(), "/queue/reply", new WebSocketNotification(
+						"nc-" + labSystem.getApiKey(), null, null,
+						thresholdUtils.createLabSystemStatusByThresholdNonConformity(thresholdNonConformities)));
 			}
 		}
 	}
-	
-	public void sendDataParameterToNodeUsers(Node node, Param param) {
-		for(SimpUser s: userRegistry.getUsers()) {
+
+	public void sendDataParameterToNodeUsers(Node node, Param param, List<DataForPlot> dataForPlot, LabSystem labSystem,
+			SampleType sampleType) {
+		for (SimpUser s : userRegistry.getUsers()) {
 			User user = userRepository.findByUsername(s.getName());
-			if(user.getNode().getId() == node.getId()) {
+			if (user.getNode().getId() == node.getId()) {
 				// send message
-				messagingTemplate.convertAndSendToUser(s.getName(), "/queue/reply", new WebSocketNotification("param", param));
+				messagingTemplate.convertAndSendToUser(s.getName(), "/queue/reply",
+						new WebSocketNotification("data-" + param.getqCCV(), labSystem.getApiKey(),
+								sampleType.getQualityControlControlledVocabulary(), dataForPlot));
 			}
 		}
 	}
-	
-	
+
+	public void sendThresholdToNodeUsers(Node node, Param param, ThresholdForPlotImpl threshold, LabSystem labSystem,
+			SampleType sampleType) {
+		for (SimpUser s : userRegistry.getUsers()) {
+			User user = userRepository.findByUsername(s.getName());
+			if (user.getNode().getId() == node.getId()) {
+				// send message
+				messagingTemplate.convertAndSendToUser(s.getName(), "/queue/reply",
+						new WebSocketNotification("threshold-" + param.getqCCV(), labSystem.getApiKey(),
+								sampleType.getQualityControlControlledVocabulary(), threshold));
+			}
+		}
+	}
 
 }
