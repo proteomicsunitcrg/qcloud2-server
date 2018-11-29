a39c2efbd85b8acab10caf754295381a4b66690f
Daniel Mancera <daniel.mancera@crg.eu>
2018-11-29 09:56:33 +0100

    initial non conformity structure

---
+++ src/main/java/eu/qcloud/nonconformity/annotation/Annotation.java
@@ -0,0 +1,27 @@
+package eu.qcloud.nonconformity.annotation;
+
+import java.util.Date;
+
+public class Annotation {
+	
+	private Long id;
+	
+	private Date date;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
